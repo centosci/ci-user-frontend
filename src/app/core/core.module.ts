e1f699022e32d732135a5e8792e7382655afca2d
Faisal Zulfiqar <faisalzulfiqar1337@gmail.com>
2019-04-06 07:43:01 +0500

    feat: Implement generic api service in core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule {
+  // make sure CoreModule is only imported once
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
